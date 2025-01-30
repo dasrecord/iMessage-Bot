5eb7c5cf5fff037b541e624a6daad2427ff9cfb7
Prasenjit Das <prasun.das.89@gmail.com>
2025-01-30 00:08:34 -0600

    Refactor iMessage bot configuration and implement safe mode for message sending

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
-message = """
+message_template = """
 Hey, {name}.
 Here's my latest video upload:
 """
+
+safe_mode = True
+
+# imessage_app (180,868)
+new_message = (472,50)
+to = (560,50)
+# message_box = (582,806)
--- pb_imessage_bot.py
+++ pb_imessage_bot.py
@@ -2,22 +2,19 @@ import pyautogui
 import requests
 import time
 import config
-
-# imessage_app (180,868)
-new_message = (472,50)
-to = (560,50)
-# message_box = (582,806)
+import datetime
 
 def short_wait():
     time.sleep(1)
+
 def long_wait():
     time.sleep(3)
 
 def get_contact_list(mode):
     contact_lists = {
-        '0': '/contact_lists/testing.csv',
-        '1': '/contact_lists/main.csv',
-        '2': '/contact_lists/vip.csv'
+        '0': 'contact_lists/testing.csv',
+        '1': 'contact_lists/main.csv',
+        '2': 'contact_lists/vip.csv'
     }
     if mode not in contact_lists:
         print(f"Invalid mode: {mode}. Please select a valid mode.")
@@ -43,26 +40,31 @@ def ping(msg):
         pass
     print(msg)
 
-
 def text_blast(name, number, message):
     # if mouse is moved to the top right corner, the program will stop
     if pyautogui.position() == (0,0):
-        ping("iMessage Bot has been stopped")
+        ping("iMessage Bot interrupted")
         exit()
-    pyautogui.click(new_message)
-    short_wait()
-    pyautogui.click(to)
-    short_wait()
-    pyautogui.write(str(number))
-    short_wait()
-    pyautogui.press('enter')
-    short_wait()
-    pyautogui.press('enter')
-    short_wait()
-    pyautogui.write(message,0.01)
-    long_wait()
-    pyautogui.press('enter')
-    short_wait()
+    # if not in safe_mde, the message will be sent
+    if config.safe_mode == False:
+        pyautogui.click(config.new_message)
+        short_wait()
+        pyautogui.click(config.to)
+        short_wait()
+        pyautogui.write(str(number))
+        short_wait()
+        pyautogui.press('enter')
+        short_wait()
+        pyautogui.press('enter')
+        short_wait()
+        pyautogui.write(message,0.01)
+        long_wait()
+        pyautogui.press('enter')
+        short_wait()
+        return True
+    else:
+        print('SAFE MODE: Message not sent to ', name, 'at ', number)
+        return False
 
 def main(contact_list):
     with open(contact_list) as f:
@@ -70,15 +72,15 @@ def main(contact_list):
         for line in lines:
             name = line.split(',')[0].split(' ')[0]
             number = line.split(',')[1]
-            formatted_message = config.message.format(name=name)
-            text_blast(name, number, formatted_message)
-            with open('just_sent.csv', 'a') as f:
-                f.write(name + ',' + number)
-            print("________________________________")
-            print('Message sent to: ', name, 'at ', number)
+            formatted_message = config.message_template.format(name=name)
+            if text_blast(name, number, formatted_message):
+                with open('just_sent.csv', 'a') as f:
+                    f.write(name + ',' + number + ',' + str(datetime.datetime.now()) + '\n')
+                print("________________________________")
+                print('Message sent to: ', name, 'at ', number)
 
 if __name__ == '__main__':
-    ping("iMessage Bot is running")
+    ping("iMessage Bot started")
     main(get_contact_list(mode_select()))
-    ping("iMessage Bot has finished running")
+    ping("iMessage Bot finished")
 
