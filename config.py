89be80a75ec96b9cbc0f0a1b968e8a6b94327ac0
Prasenjit Das <prasun.das.89@gmail.com>
2025-01-30 00:55:13 -0600

    fix typos

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ Here's my latest video:
 
 safe_mode = True
 
-# imessage_app (180,868)
+imessage_app = (180,868)
 new_message_box = (472,50)
 recipient_box = (560,50)
-# message_box = (582,806)
+message_box = (582,806)
