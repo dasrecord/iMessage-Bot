671df6b6aa0bf615ea75ccf6b6736cc85cc64154
Prasenjit Das <prasun.das.89@gmail.com>
2025-01-30 18:24:30 -0600

    Update message box coordinates for improved layout in iMessage bot

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ Here's my latest video:
 safe_mode = True
 
 imessage_app = (180,868)
-new_message_box = (472,50)
-recipient_box = (560,50)
-message_box = (582,806)
+new_message_box = (473,48)
+recipient_box = (546,51)
+message_box = (576,815)
